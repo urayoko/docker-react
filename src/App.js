fcd5008316549ab7fda612c29b06cc03628187ed
WORKSTATION\yoko <urayoko@yahoo.com>
2020-01-07 21:20:47 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          Hi 13, Edit <code>src/App.js</code> and save to reload.
+        </p>    
         <a
           className="App-link"
           href="https://reactjs.org"
